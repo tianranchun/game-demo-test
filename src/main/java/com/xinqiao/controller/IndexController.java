6db637fab5ea25b754a808e68433c5ff5286e5ff
dajian <779151588@qq.com>
2017-10-20 21:15:07 +0800

    youhuatijiao

---
+++ src/main/java/com/xinqiao/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.xinqiao.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index")
+	public String list(HttpServletRequest request){
+		
+		return "index";
+	}
+	
+}
